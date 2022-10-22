61bdaee6362e6a211f761de03eca939e3338e60f
Ramkumar <ram1070@gmail.com>
2022-10-22 11:49:02 +0530

    Folder struture created

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 from typing import List
-   
 
 #declaring variable for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.6"
 AUTHOR="Ramkumar"
 DESCRIPTION="This is a first FSDS Nov  batch Machine laerning project"
-PACKAGES=["housing"]
+HYPHEN_E_DOT = "-e ."
 REQUIREMENTS_FILE_NAME='requirements.txt'
-
 
 def get_requirements_list()->List[str]:
     """
@@ -18,13 +16,15 @@ def get_requirements_list()->List[str]:
     return This function is going to return  a list which contain name of libraries  mentioned in requirement.txt file
     """
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
-
+        requirement_list=[requirement.replace("\n","") for requirement in requirement_file.readlines()]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
